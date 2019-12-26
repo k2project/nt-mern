a4b760672110af7ea861a69a6811902fff7d9287
k2project <kopczkrzy@gmail.com>
2019-12-26 15:36:55 +0000

    mao ipad update

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -236,12 +236,14 @@ const Directions=()=>{
                 </div>
                 <div className="Directions__bg_map">
                     <div className="cover">
-                        Find directions with Google Maps*
+                        <div className="cover_ctr">
+                            Find directions with Google Maps*
 
-                        <a href="https://www.google.com/maps/dir//Tokenhouse+Yard,+London+EC2R+7AS/@51.5153851,-0.0907049,17z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x48761cab515b7a57:0xa9652d3756affe10!2m2!1d-0.0885162!2d51.5153851!3e3"  target="_blank" rel="noopener noreferrer" className="btn">Tokenhouse Yard - City of London</a>
-                        <a
-                             href="https://www.google.com/maps/dir//Nassau+St,+Fitzrovia,+London+W1W+7AF/@51.5188268,-0.1412905,17z/data=!4m17!1m7!3m6!1s0x48761b2a47340f1d:0x7122f29f93bdb1e8!2sNassau+St,+Fitzrovia,+London+W1W+7AF!3b1!8m2!3d51.5188268!4d-0.1391018!4m8!1m0!1m5!1m1!1s0x48761b2a47340f1d:0x7122f29f93bdb1e8!2m2!1d-0.1391018!2d51.5188268!3e3"  target="_blank" rel="noopener noreferrer" className="btn">Nassau Street – West End</a>
-                        <p><small><i>*Directions open up in a new tab.</i></small></p>
+                            <a href="https://www.google.com/maps/dir//Tokenhouse+Yard,+London+EC2R+7AS/@51.5153851,-0.0907049,17z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x48761cab515b7a57:0xa9652d3756affe10!2m2!1d-0.0885162!2d51.5153851!3e3"  target="_blank" rel="noopener noreferrer" className="btn">Tokenhouse Yard - City of London</a>
+                            <a
+                                 href="https://www.google.com/maps/dir//Nassau+St,+Fitzrovia,+London+W1W+7AF/@51.5188268,-0.1412905,17z/data=!4m17!1m7!3m6!1s0x48761b2a47340f1d:0x7122f29f93bdb1e8!2sNassau+St,+Fitzrovia,+London+W1W+7AF!3b1!8m2!3d51.5188268!4d-0.1391018!4m8!1m0!1m5!1m1!1s0x48761b2a47340f1d:0x7122f29f93bdb1e8!2m2!1d-0.1391018!2d51.5188268!3e3"  target="_blank" rel="noopener noreferrer" className="btn">Nassau Street – West End</a>
+                            <p><small><i>*Directions open up in a new tab.</i></small></p>
+                        </div>
                     </div>
                 </div>
         </section>
