ef99145477006593dae6587eb25a51bb6d6aa632
k2project <kopczkrzy@gmail.com>
2020-03-27 12:32:08 +0000

    COVID19 banner

--- client/src/components/banner/Banner.js
+++ client/src/components/banner/Banner.js
@@ -11,7 +11,7 @@ const Banner =()=>{
                 <div className="Banner__body">
                     <div>
                         <p>The coronavirus brings additional stress. It threatens our ability as people to meet and support one another. We have to be creative and flexible in responding to this crisis. These are unusual times.</p>
-                        <p>Technology can contribute to an evolution in how people receive therapy and so during this time of uncertainty I will be offering all my counselling and psychotherapy sessions online. </p>
+                        <p>Technology can contribute to an evolution in how people receive therapy and during this time of uncertainty I will be offering all my counselling and psychotherapy sessions online. </p>
                         <button className="btn" onClick={()=>document.getElementById('banner').remove()}>OK</button>
                     </div>
                     <div>
