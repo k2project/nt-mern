2e2b7d5cb200a172c5a4e0627395fd9286001e9f
k2project <kopczkrzy@gmail.com>
2020-04-01 12:56:15 +0100

    mail server json error

--- routes/mail.js
+++ routes/mail.js
@@ -1,83 +1,97 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 module.exports = (function() {
-    'use strict';
-    const mailRoutes = require('express').Router();
+    "use strict";
+    const mailRoutes = require("express").Router();
 
-    mailRoutes.post('/send', function (req, res) {
+    mailRoutes.post("/send", function(req, res) {
         const { name, mail, mobile, location } = req.body;
         if (!name) {
             return res.send({
                 success: false,
-                message: 'Your name is required.'
+                message: "Your name is required."
             });
-       }
+        }
         if (!mail && !mobile) {
             return res.send({
                 success: false,
-                message: 'Please enter at last one way to contact you or call me directly on  0208 675 9754.'
+                message:
+                    "Please enter at last one way to contact you or call me directly on  0208 675 9754."
             });
-       }
-       const emailRegex =/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-       if (mail&&!emailRegex.test(mail)) {
-           return res.send({
-               success: false,
-               message: 'Please enter a valid address or send an emial directly to georgie@calvocoressi.com.'
-           });
-      }
-       const mobRegex=/^[+]?(\d+([- ])?)*$/;
-       if (mobile&&!mobRegex.test(mobile)) {
-           return res.send({
-               success: false,
-               message: 'Please enter a valid mobile number or call me directly on  02086759754.'
-           });
-      }
-      console.log(location)
-       if (!location) {
-           return res.send({
-               success: false,
-               message: 'Please indicate at what place you wish to see me.'
-           });
-      }
+        }
+        const emailRegex = /[a-z0-9!#$%&"*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&"*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        if (mail && !emailRegex.test(mail)) {
+            return res.send({
+                success: false,
+                message:
+                    "Please enter a valid address or send an emial directly to georgie@calvocoressi.com."
+            });
+        }
+        const mobRegex = /^[+]?(\d+([- ])?)*$/;
+        if (mobile && !mobRegex.test(mobile)) {
+            return res.send({
+                success: false,
+                message:
+                    "Please enter a valid mobile number or call me directly on  02086759754."
+            });
+        }
+        console.log(location);
+        if (!location) {
+            return res.send({
+                success: false,
+                message: "Please indicate at what place you wish to see me."
+            });
+        }
 
-         //secure: true for 465, false for other ports
-            let transporter = nodemailer.createTransport({
-               host: 'smtp.livemail.co.uk',
-               port: 465,
-               secure: true,
-               auth: {
-                   user: process.env.MAIL_USER,
-                   pass: process.env.MAIL_PASS
-               },
-               // tls: {
-               //      rejectUnauthorized: false
-               //  }
-           });
+        //secure: true for 465, false for other ports
+        let transporter = nodemailer.createTransport({
+            host: "smtp.livemail.co.uk",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            },
+            // tls: {
+            //      rejectUnauthorized: false
+            //  }
+        });
 
-            let text= req.body.name+' is requesting a meeting at '+req.body.location+' .'
-            text+= req.body.mail? ' Email them back by clicking on the "Reply" button':'';
-            let replyTo=  req.body.mail?  req.body.mail:'';
-            text+= (req.body.mobile && !req.body.mail)? ' Contact them directly only by mobile on '+req.body.mobile:'';
-            text+= (req.body.mobile && req.body.mail)? ' or call '+req.body.mobile:'';
-            text+='.';
-            const mailOptions = {
-              from: process.env.MAIL_USER,
-              to: process.env.MAIL_USER,
-              replyTo,
-              subject: 'Consultaion request received.',
-              text
-            };
+        let text =
+            req.body.name +
+            " is requesting a meeting at " +
+            req.body.location +
+            " .";
+        text += req.body.mail
+            ? " Email them back by clicking on the 'Reply' button"
+            : "";
+        let replyTo = req.body.mail ? req.body.mail : "";
+        text +=
+            req.body.mobile && !req.body.mail
+                ? " Contact them directly only by mobile on " + req.body.mobile
+                : "";
+        text +=
+            req.body.mobile && req.body.mail
+                ? " or call " + req.body.mobile
+                : "";
+        text += ".";
+        const mailOptions = {
+            from: process.env.MAIL_USER,
+            to: process.env.MAIL_USER,
+            replyTo,
+            subject: "Consultation request received.",
+            text
+        };
 
-            transporter.sendMail(mailOptions, function(error, info){
-              if (error) {
-                // console.log(error);
-              } else {
-                console.log('Email sent: ' + info.response);
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Email sent: " + info.response);
                 res.send({
-                   success: true,
-
-               });
-              }
-            });
+                    success: true
+                });
+            }
+        });
     });
 
     return mailRoutes;
