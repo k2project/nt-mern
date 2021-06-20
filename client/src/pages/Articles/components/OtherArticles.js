bdc53270472e48ca5f693c8b35c4b09caf3c38fd
k2projct <kopczkrzy@gmail.com>
2021-06-20 10:27:38 +0100

    limited other articles to 4 per page with show more button

--- client/src/pages/Articles/components/OtherArticles.js
+++ client/src/pages/Articles/components/OtherArticles.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { articles } from './articles';
 
 import { ArticleRow } from './ArticleRow';
 
+const LIMIT_ARTICLES_DISPLAY = 4;
+
 export const OtherArticles = () => {
+    const [rest, showRest] = useState(false);
+    const others = articles
+        .filter((a) => !a.featured)
+        .sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    const initShownArticles = others.slice(0, LIMIT_ARTICLES_DISPLAY);
+    const restArticles = others.slice(LIMIT_ARTICLES_DISPLAY);
+
     return (
         <div className='Articles-others'>
-            {articles
-                .filter((a) => !a.featured)
-                .sort((a, b) => new Date(b.date) - new Date(a.date))
-                .map((a, i) => (
+            {initShownArticles.map((a, i) => (
+                <ArticleRow key={a.id} article={a} id={a.id} />
+            ))}
+            {!rest && (
+                <div className='Articles-others-btnContainer'>
+                    <button onClick={() => showRest(true)} className='btn'>
+                        Show more
+                    </button>
+                </div>
+            )}
+            {rest &&
+                restArticles.map((a, i) => (
                     <ArticleRow key={a.id} article={a} id={a.id} />
                 ))}
         </div>
