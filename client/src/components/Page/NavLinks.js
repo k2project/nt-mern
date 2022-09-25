5439961802a8fc6a6e85c518aee1b5c187d66e28
Kris <kopczkrzy@gmail.com>
2022-09-25 15:49:32 +0100

    Added article to blog page: A safe space to change from insideTime

--- client/src/components/Page/NavLinks.js
+++ client/src/components/Page/NavLinks.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 export default function NavLinks({ toggleMobileMenu }) {
---
+++ client/src/pages/Articles/components/articles/ASafeSpaceToChange.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import OuterLink from '../../../../components/links/OuterLink';
+import { TRUTH_PROJECT, IICSA } from '../../../../config';
+
+const truthProjectLink = (
+    <OuterLink
+        href={TRUTH_PROJECT.urlHome}
+        text={`The ${TRUTH_PROJECT.name}`}
+    />
+);
+const iicsaLink = <OuterLink href={IICSA.urlHome} text={IICSA.name} />;
+
+export default {
+    id: 'ASafeSpaceToChange',
+    cls: 'ASafeSpaceToChange',
+    featured: false,
+    category: 'Opinion piece',
+    title:
+        'A safe space to change',
+    summary:
+        'The only Democratic Therapeutic Community in a UK women’s prison is housed at HMP Send, in Surrey. But what happens there? ',
+    date: '2022-09-02',
+    author: 'Nujoji Calvocoressi',
+    publication: 'insideTime',
+    sourceUrl:
+        'https://insidetime.org/a-safe-space-to-change/',
+    img: 'dtc.png',
+    logo: 'insidetime.png',
+    content: (
+        <>
+         <h4 className='article-subtitle'>The only Democratic Therapeutic Community in a UK women’s prison is housed at HMP Send, in Surrey. But what happens there? </h4>
+            <h5 className='article-subtitle-small'>
+            What is the DTC?
+            </h5>
+            <p>
+            The Democratic Therapeutic Community (DTC) is an accredited, group-based intervention aimed at reducing risk by facilitating real, lasting change. We are a supportive environment where members live and work together to explore and challenge behaviours and attitudes which can lead to offending. We offer a safe space for members with complex psychological and emotional needs – including personality difficulties – to explore their histories. The service works in a way which is informed and equipped to manage trauma.
+            </p>
+            <h5 className='article-subtitle-small'>
+            How many women are there?
+            </h5>
+            <p>
+            The DTC has spaces for 24 residents. 
+            </p>
+            <h5 className='article-subtitle-small'>
+            How long do they stay, and where do they move on to afterwards?
+            </h5>
+            <p>
+            Members can expect to spend between 21 months and three years at the DTC so they must have enough time left on their sentence. New members spend the first three months together in an assessment group, which allows them time to settle in and get used to sharing in groups. Before members leave the DTC they are supported to prepare for relocation or resettlement. Some members transfer to an enhanced wing, some are released, and some apply to progression PIPE [Psychologically-Informed Planned Environment].  
+            </p>
+            <h5 className='article-subtitle-small'>
+            What facilities are there?
+            </h5>
+            <p>
+            At the DTC, members are all in single cells with a toilet and shower. They have access to a dining hall to eat meals in, a large community space to associate, a library, gym with exercise classes and access to the gardens and outdoor space including their own polytunnel for growing vegetables (pictured). Members also have access to a kitchen.  
+            </p>
+            <h5 className='article-subtitle-small'>
+            What is the regime like?
+            </h5>
+            <p>
+            The week begins on Monday with a community meeting. This is where issues are discussed and decisions made that affect the day-to-day life of the community. On Tuesday and Thursday morning, residents attend their small therapy group. On Wednesday, residents participate in the living and learning space where they come together to learn new life skills in an informal context. On Friday the community comes together, and members and staff give each other feedback about their progress. During the afternoon, members attend art therapy, gardens, or attend education and work.  
+            </p>
+            <p>While group therapy is an important part of the DTC, therapeutic processes also occur in communal spaces. In these spaces, members spend time together to develop healthy social relationships. Examples are watching films, cooking meals and playing games together. There is also a community breakfast held at the weekend.</p>
+            <h5 className='article-subtitle-small'>
+            How does the ‘democratic’ aspect work?
+            </h5>
+            <p>
+            One of the important principles underpinning the community is democracy. This means that as far as possible, members will have a voice in the day-to-day decision-making and running of the community. This includes voting about important decisions that impact community life – from what activities take place, to the Christmas menu.   
+            </p>
+             <h5 className='article-subtitle-small'>
+            What therapy is provided? 
+            </h5>
+            <p>
+            We are a multi-disciplinary service, and our staff team reflects this. At the DTC, we have both operational and non-operational staff, all of whom participate in service delivery. During a member’s time on the community, they will get to know and work alongside our specialist officers, psychologists, psychotherapists, management team and civilian staff.    
+            </p>
+            <p>
+            Therapy takes place in smaller groups, and residents will focus on making links between their behaviour in prison and their criminal behaviour and early life experiences. This may include exploring relationships, unresolved family conflicts, and working through early traumatic experiences. 
+            </p>
+             <h5 className='article-subtitle-small'>
+            Is it only for lifers and IPPs?
+            </h5>
+            <p>
+            We are a national service and accept referrals from across the women’s prison estate. Any adult woman who has been sentenced can apply or be referred to the DTC. Every application is considered on an individual basis.   
+            </p>
+            <p>
+            If you would like to know more, please talk to your prison offender manager, who will be able to give you one of our information leaflets, or an expression of interest form. You can also write to us at: Head of Psychotherapy, HMP Send, Ripley Road, Woking, Surrey GU23 4LJ. Our email address is TC.Send@justice.gov.uk
+            </p>
+            <p><i>Nujoji Calvocoressi is a Group Psychotherapist in the DTC at HMP Send</i></p>
+         
+        </>
+    ),
+    comments: [],
+};
